e55c05bcaffb58133ca0e88d9484ac3cd854600a
fairyname-rgb <fairyname17@gmail.com>
2020-06-28 01:33:41 +0300

    Updates

---
+++ precache-manifest.1e17d30b71ad22fd3be5774f7bf6762c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0830ef17f1471774082196281bff6b68",
+    "url": "/react-task-app/index.html"
+  },
+  {
+    "revision": "177d8ebd91575d2db687",
+    "url": "/react-task-app/static/css/main.baaf5daf.chunk.css"
+  },
+  {
+    "revision": "be49fb466e0653eeb814",
+    "url": "/react-task-app/static/js/2.55e1f384.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-task-app/static/js/2.55e1f384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177d8ebd91575d2db687",
+    "url": "/react-task-app/static/js/main.0d87bfc8.chunk.js"
+  },
+  {
+    "revision": "e3c407072ee412dbc9c2",
+    "url": "/react-task-app/static/js/runtime-main.070d4d05.js"
+  }
+]);
